2e2950dcbd19aaa13f160c081d33a85515e70c45
MAC <mac@MACs-MacBook-Pro.local>
2025-04-26 12:21:10 +0100

    edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
       </ul>
 
       {/* only for mobile devices, created separately */}
-      <div className="sm:hidden flex flex-1 justify-end items-center">
+      <div className="sm:hidden z-[999] flex flex-1 justify-end items-center">
         {/* shows toggle icon based on its state */}
         <img
           src={toggle ? close : menu}
